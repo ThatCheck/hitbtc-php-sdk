254e8690a1481b2a85c5019301a1407fa248a4df
Alexandru Cobuz <alexandru.cobuz@gmail.com>
2017-11-13 01:41:05 +0200

    Added getSymbols method, fixed typo
    - added method getSymbols, it's in the API docs
    - fixed typo, getOrderBook instead of getOderBook

--- src/Hitbtc/PublicClient.php
+++ src/Hitbtc/PublicClient.php
@@ -43,8 +43,13 @@ class PublicClient
         return json_decode($this->getHttpClient()->get('/api/1/public/ticker')->getBody(), true);
     }
    
-    public function getOderBook($ticker)
+    public function getOrderBook($ticker)
     {
         return json_decode($this->getHttpClient()->get('/api/1/public/'.$ticker.'/orderbook')->getBody(), true);
+    }
+    
+    public function getSymbols()
+    {
+        return json_decode($this->getHttpClient()->get('/api/1/public/symbols')->getBody(), true);
     }
 }
